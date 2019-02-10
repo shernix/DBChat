384c37c29eb422df3886c7faaf8975ad0380dac0
javier <javier.velez12@upr.edu>
2019-02-10 14:42:03 -0400

    Created a default layout for all routes

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
-#this is a test file
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
