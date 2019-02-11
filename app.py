07836aa5ff86dcc67a0a4815934f7ab614e87082
javier <javier.velez12@upr.edu>
2019-02-11 12:56:59 -0400

    Updated a layout for all routes

--- app.py
+++ app.py
@@ -9,6 +9,21 @@ def index():
     return render_template('home.html')
 
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+
+@app.route('/messaging')
+def messaging():
+    return render_template('messaging.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
