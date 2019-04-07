564f4dd4423229ef9a4bd7b6870da62a0366573c
shernix <eduardo.santiago8@upr.edu>
2019-04-07 14:08:32 -0400

    gitignore

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,13 +1,7 @@
 # Databse configuration information
 
 pg_config = {
-<<<<<<< HEAD
     'user' : 'superuser',
     'passwd' : 'kheapp',
     'dbname' : 'kheapp'
-=======
-    'user' : 'appuser',
-    'passwd' : 'DBChat2',
-    'dbname' : 'appdb'
->>>>>>> e910e94f34d20565d905bec07ec93a3fcc9cf4bf
 }
